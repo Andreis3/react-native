5d8fd6757c5bed63dd2755a88c299ebf7452b4a0
Andreis3 <andrei.as@hotmail.com>
2019-08-21 00:07:37 -0300

    React Navigation

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {View} from 'react-native';
 
 import './config/ReactotronConfig';
 
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+import Routes from './routes';
 
-console.tron.log('Hello World');
 export default function App() {
-  return (
-    <View style={style.container}>
-      <Text style={style.welcome}>welcome to React Native</Text>
-      <Text style={style.welcome}>Abaixo</Text>
-    </View>
-  );
+  return <Routes />;
 }

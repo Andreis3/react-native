008f95e764a9938ccc6c983d6822cea275ea9b70
Andreis3 <andrei.as@hotmail.com>
2019-08-24 22:47:20 -0300

    add loading bar

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ProgressBarAndroid } from 'react-native';
 import PropTypes from 'prop-types';
 
 import api from '../../services/api';
@@ -19,7 +20,7 @@ import {
 // import { Container } from './styles';
 
 export default class User extends Component {
-  static navigationOptions = ({navigation}) => ({
+  static navigationOptions = ({ navigation }) => ({
     title: navigation.getParam('user').name,
   });
 
@@ -33,39 +34,50 @@ export default class User extends Component {
   // eslint-disable-next-line react/state-in-constructor
   state = {
     stars: [],
+    loading: false,
   };
 
   async componentDidMount() {
-    const {navigation} = this.props;
+    const { navigation } = this.props;
     const user = navigation.getParam('user');
+    this.setState({ loading: true });
     const response = await api.get(`/users/${user.login}/starred`);
-    this.setState({stars: response.data});
+    this.setState({ stars: response.data });
+    this.setState({ loading: false });
   }
 
   render() {
-    const {navigation} = this.props;
-    const {stars} = this.state;
+    const { navigation } = this.props;
+    const { stars, loading } = this.state;
     const user = navigation.getParam('user');
     return (
       <Container>
         <Header>
-          <Avatar source={{uri: user.avatar}} />
+          <Avatar source={{ uri: user.avatar }} />
           <Name>{user.name}</Name>
           <Bio>{user.bio}</Bio>
         </Header>
-        <Stars
-          data={stars}
-          keyExtractor={star => String(star.id)}
-          renderItem={({item}) => (
-            <Starred>
-              <OwnerAvatar source={{uri: item.owner.avatar_url}} />
-              <Info>
-                <Title>{item.name}</Title>
-                <Author>{item.owner.login}</Author>
-              </Info>
-            </Starred>
-          )}
-        />
+        {loading ? (
+          <ProgressBarAndroid
+            styleAttr="Horizontal"
+            indeterminate
+            color="#7859c1"
+          />
+        ) : (
+          <Stars
+            data={stars}
+            keyExtractor={star => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
       </Container>
     );
   }
